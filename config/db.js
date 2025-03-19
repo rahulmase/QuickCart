57b02072d311b44e8cdbaa53155d3f1961e327a5
rahul <rahulmauryapp90@gmail.com>
2025-03-19 09:51:26 +0530

    Resolved merge conflict in config/db.js

--- config/db.js
+++ config/db.js
@@ -24,13 +24,12 @@ async function connnectDB()
 
         cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose=> {
             return mongoose
-<<<<<<< HEAD
         })        
-=======
-        })
+        //})
+    
+
             
         
->>>>>>> eb06f3252278edea2fb2d68b0c4fe31864d4dc5b
     }
 
     cached.conn = await cached.promise
