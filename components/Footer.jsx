47f6e06971c0b9e1c4ef421d4f80b3a3d5282616
rahulmase <rahulmauryapp90@gmail.com>
2025-03-24 08:33:45 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -40,8 +40,8 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>+91-9874561200</p>
+              <p>contact @ Rahulstack.in</p>
             </div>
           </div>
         </div>
